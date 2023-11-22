1e7d6b3d38f3e4f61a8b5a8376028d201495ff96
imrulkaisar <imrulkaisar3@gmail.com>
2023-11-22 17:28:24 +0600

    Added user context

--- src/Contexts/UserContext.jsx
+++ src/Contexts/UserContext.jsx
@@ -1,9 +1,65 @@
-import { createContext } from "react";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import auth from "../Authentication/Firebase.config";
 
 export const UserContext = createContext(null);
 
 const UserContextProvider = ({ children }) => {
-  const authInfo = {};
+  const [user, setUser] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const logInWithEmailAndPassword = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider);
+  };
+
+  const updateUser = (updateData) => {
+    updateProfile(auth.currentUser, updateData);
+  };
+
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+        console.log("User logged in: ", currentUser);
+        setLoading(false);
+      } else {
+        console.log("User logged out!");
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    logInWithEmailAndPassword,
+    signInWithGoogle,
+    updateUser,
+    logOut,
+  };
   return (
     <UserContext.Provider value={authInfo}>{children}</UserContext.Provider>
   );
